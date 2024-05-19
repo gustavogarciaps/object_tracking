894f4c20c108323351734ff5d18b7f17a1b635f4
Gustavo Garcia <sousagustavogarcia@gmail.com>
2024-05-19 13:14:51 -0300

    feat: add box limited

--- main_count.py
+++ main_count.py
@@ -7,8 +7,9 @@ cap = cv2.VideoCapture("assets/video.mp4")
 assert cap.isOpened(), "Error reading video file"
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
-# Define region points as a polygon with 5 points
-region_points = [(20, 400), (1080, 404), (1080, 360), (20, 360), (20, 400)]
+# Define line points
+line_points = [(20, 400), (1200, 400)]
+classes_to_count = [2]  # person and car classes for count
 
 # Video writer
 video_writer = cv2.VideoWriter("object_counting_output.avi",
@@ -19,7 +20,7 @@ video_writer = cv2.VideoWriter("object_counting_output.avi",
 # Init Object Counter
 counter = object_counter.ObjectCounter()
 counter.set_args(view_img=True,
-                 reg_pts=region_points,
+                 reg_pts=line_points,
                  classes_names=model.names,
                  draw_tracks=True,
                  line_thickness=2)
@@ -29,7 +30,7 @@ while cap.isOpened():
     if not success:
         print("Video frame is empty or video processing has been successfully completed.")
         break
-    tracks = model.track(im0, persist=True, show=False)
+    tracks = model.track(im0, persist=True, show=False, classes=classes_to_count)
 
     im0 = counter.start_counting(im0, tracks)
     # video_writer.write(im0)
